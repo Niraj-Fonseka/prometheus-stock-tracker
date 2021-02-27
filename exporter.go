d034408a9c6f81a33d6abfe21edab153a010ca75
Niraj-Fonseka <fonseka.live@gmail.com>
2021-02-27 02:19:30 -0600

    starting up go exporter

---
+++ exporter.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			g.Set(rand.Float64())
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	g = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "random_float_metric",
+		Help: "The total number of processed events",
+	})
+)
+
+func main() {
+	recordMetrics()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
